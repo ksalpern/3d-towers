de6738cefae873a6670cffdd5653e57d47f867a1
Kseniia <ksenia.pidopryhora@gmail.com>
2024-12-18 13:19:59 +0200

    add enemy class

--- frontend/src/game/enemy.ts
+++ frontend/src/game/enemy.ts
@@ -1,9 +1,18 @@
-// import * as BABYLON from '@babylonjs/core'
+import * as BABYLON from '@babylonjs/core'
 
-// export class Enemy {
-//   constructor({ position = { x: 0, y: 0, z: 0 }, width = 0.1, height = 0.1 }) {
-//     this.position = position
-//     this.width = width
-//     this.height = height
-//   }
-// }
+export class Enemy {
+  private armor: number
+  private health: number
+  private position: BABYLON.Vector3
+  private scene: BABYLON.Scene
+  private speed: number
+  private type: string
+  private level: number
+
+  constructor(scene: BABYLON.Scene, position: BABYLON.Vector3) {
+    this.scene = scene
+    this.position = position
+  }
+
+  
+}
